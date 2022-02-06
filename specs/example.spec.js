62b508354dfef90a5d3e4fb0d8f52233b1774c78
Drayy7 <Drayy7@gmail.com>
2022-02-05 20:25:19 -0800

    add firs test

---
+++ specs/example.spec.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+describe('Math functions',function () {
+    it('A + B = C', function() {
+        const a = 4
+        const b = 5
+        const c = a + b
+ expect(c).to.eq(9)
+    })
+})
